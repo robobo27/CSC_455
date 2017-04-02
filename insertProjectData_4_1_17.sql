5ad63925bcdc2955ae722624447d463d46310967
dillonharless <drh4179@uncw.edu>
2017-04-02 13:21:37 -0400

    uploaded most recent insert file

--- insertProjectData_4_1_17.sql
+++ insertProjectData_4_1_17.sql
@@ -32,30 +32,38 @@ insert into SiteProducts
 			('09','image9',1,LOAD_FILE('image9.jpg'),3000,'36x48','Acrylic on canvas');		
 
 insert into SiteCustomers
-	values ('1111','Charles','123 Main St.','67226','316-636-555'),
-		   ('2222','Bertram','237 Ash Avenue','67226','316-689-5555'),
-		   ('3333','Barbara','111 Inwood St.','60606','316-111-1234'),
-		   ('4444','Will','111 Kenwood St.','54444','416-111-1234'),
-		   ('5555','Bill','211 Marlwood St.','28403','416-111-1235'),
-		   ('6666','Keely','211 Pinewood St.','28411','416-111-1235'),
-		   ('7777','Maera','211 Marlwood St.','28403','416-111-1235');					
+	values ('Charles@gmail.com','Charles','316-636-555'),
+		   ('Bertram@gmail.com','Bertram','316-689-5555'),
+		   ('Barbara@gmail.com','Barbara','316-111-1234'),
+		   ('Will@gmail.com','Will','416-111-1234'),
+		   ('Bill@gmail.com','Bill','416-111-1235'),
+		   ('Keely@gmail.com','Keely','416-111-1236'),
+		   ('Maera@gmail.com','Maera','416-111-1235');					
 
 insert into SiteOrders
-	values	('201','1111','1994-12-10','1994-12-12'),
-			('202','1111','1995-01-12','1995-01-15'),
-			('203','2222','1995-02-13','1995-02-20'),
+	values	('201','Charles@gmail.com','1994-12-10','1994-12-12'),
+			('202','Barbara@gmail.com''1995-01-12','1995-01-15'),
+			('203','Charles@gmail.com','1995-02-13','1995-02-20'),
 			('204','3333','2003-02-15','NULL'),
-			('205','4444','2003-02-15','2003-02-16'),
+			('205','Charles@gmail.com','2003-02-15','2003-02-16'),
 			('206','5555','2003-02-15','2003-02-16');
 			
 insert into SiteOdetails
 	values	('201','01',1),
 			('202','03',1),
-			('203','04',2),
-			('204','05',3),
-			('205','06',4),
-			('206','07',7);
+			('203','04',1),
+			('204','05',1),
+			('205','06',1),
+			('206','07',1);
 
+insert into LivesAt
+	values ('Charles@gmail.com','4581 Cherry Street','67226'),
+		   ('Bertram@gmail.com','3321 Columbus Avenue','60606'),
+		   ('Barbara@gmail.com','1234 Number Lane','50302'),
+		   ('Will@gmail.com','387 Short Street','54444'),
+		   ('Bill@gmail.com','7098 Dismy Court','66002'),
+		   ('Keely@gmail.com','2121 Jump Street','61111'),
+		   ('Maera@gmail.com','1051 Page Avenue','28403');
 			
 			
 			
