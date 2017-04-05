218ec1946cca67d76e9ca0cea392f26139baee4c
Robert Arnold <rarnoldiv@gmail.com>
2017-04-05 16:49:55 -0400

    Dropped SiteAddresses and modified LivesAt

--- PTables455.sql
+++ PTables455.sql
@@ -1,13 +1,12 @@
 -- no-auto-rehash
 
-use drh4179
+use rla8371
 
 drop table if exists LivesAt;
 drop table if exists OurOdetails;
 drop table if exists OurOrders;
 drop table if exists OurCustomers;
 drop table if exists OurProducts;
-drop table if exists OurAddresses;
 
 
 CREATE TABLE OurProducts (PNO varchar(6),
@@ -18,47 +17,39 @@ CREATE TABLE OurProducts (PNO varchar(6),
 					   DIMENSIONS varchar (10),
 					   MEDIUM varchar(50),
                        PRIMARY KEY (PNO)) ENGINE=INNODB;
+
+CREATE TABLE OurCustomers (CEMAIL varchar(40),
+					   CNAME varchar(20),
+					   PHONE varchar(15),
+                       PRIMARY KEY(CEMAIL)) ENGINE=INNODB;
 					   
-CREATE TABLE OurAddresses (STREETADD varchar (30),
+CREATE TABLE LivesAt (CEMAIL varchar(40),
+					   STREETADD varchar (30),
 					   ZIP varchar (6),
 					   CITY varchar (20),
 					   CSTATE varchar (30),
-					   PRIMARY KEY (STREETADD, ZIP)) ENGINE=INNODB;
+					   PRIMARY KEY (CEMAIL,STREETADD,ZIP), 
+					   FOREIGN KEY(CEMAIL)
+					   REFERENCES OurCustomers(CEMAIL)
+					   ON UPDATE CASCADE ON DELETE CASCADE) ENGINE=INNODB;
 					   
-
-CREATE TABLE OurCustomers (CEMAIL varchar(40),
-                      CNAME varchar(20),
-					  PHONE varchar(15),
-                      PRIMARY KEY(CEMAIL)) ENGINE=INNODB;
 					  
 CREATE TABLE OurOrders (ONO int not null AUTO_INCREMENT,
-                      CEMAIL varchar(40),
-					  RECEIVED date,
-					  SHIPPED date,
-                      PRIMARY KEY(ONO),
-                      FOREIGN KEY (CEMAIL)
-                      REFERENCES OurCustomers(CEMAIL)
-                      ON UPDATE CASCADE ON DELETE CASCADE) ENGINE=INNODB;
+                       CEMAIL varchar(40),
+					   RECEIVED date,
+					   SHIPPED date,
+                       PRIMARY KEY(ONO),
+                       FOREIGN KEY (CEMAIL)
+                       REFERENCES OurCustomers(CEMAIL)
+                       ON UPDATE CASCADE ON DELETE CASCADE) ENGINE=INNODB;
 					  
-CREATE TABLE OurOdetails(ONO int not null,
-                      PNO varchar(6),
-                      QTY int not null,
-					  PRIMARY KEY(ONO),
-                      FOREIGN KEY(ONO)
-					  REFERENCES OurOrders(ONO)        
-                      ON UPDATE CASCADE ON DELETE CASCADE,
-					  FOREIGN KEY(PNO)
-					  REFERENCES OurProducts(PNO)
-					  ON UPDATE CASCADE ON DELETE RESTRICT) ENGINE=INNODB;
-					  
-/* Took out ZIP from FOREIGN KEY */					  
-CREATE TABLE LivesAt(CEMAIL varchar(40),
-					  STREETADD varchar(30),
-					  ZIP varchar(6),
-					  PRIMARY KEY(CEMAIL,STREETADD,ZIP),
-					  FOREIGN KEY(CEMAIL) 
-					  REFERENCES OurCustomers(CEMAIL),
-					  FOREIGN KEY(STREETADD)
-					  REFERENCES OurAddresses(STREETADD)
-                      ON UPDATE CASCADE ON DELETE CASCADE)	ENGINE=INNODB;
-					  
+CREATE TABLE OurOdetails (ONO int not null,
+                       PNO varchar(6),
+                       QTY int not null,
+					   PRIMARY KEY(ONO),
+                       FOREIGN KEY(ONO)
+					   REFERENCES OurOrders(ONO)        
+                       ON UPDATE CASCADE ON DELETE CASCADE,
+					   FOREIGN KEY(PNO)
+					   REFERENCES OurProducts(PNO)
+					   ON UPDATE CASCADE ON DELETE RESTRICT) ENGINE=INNODB;
