01f9147492d0de6444fb795380160e72c3f949cf
dillonharless <drh4179@uncw.edu>
2017-04-17 13:30:25 -0400

    asd

---
+++ createView.sql
@@ -0,0 +1,9 @@
+/* Create View */ 
+
+-- no-auto-rehash
+
+
+/* Allows to see all shipped and received dates to get an average idea of delivery time. */
+   
+create view Deliveries as select RECEIVED,SHIPPED
+from OurOrders;
