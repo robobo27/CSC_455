064b3e8271b1cf83b7bf05ac9b5a27455ffc46a6
dillonharless <drh4179@uncw.edu>
2017-04-19 12:35:26 -0400

    nested subquery

--- nestedSubq.sql
+++ nestedSubq.sql
@@ -1,6 +1,6 @@
-/* Nested subquery that shows who has not ordered anything */
+/* Nested subquery that shows orders over 1000 dollars */
 
-select CEMAIL from(
-select * from OurCustomers as OurCustomers2
-natural left outer join OurOrders as OurOrders2 where OurOrders2.ONO is null)
-as WhoHasNotOrdered;
+create or replace view OrdersUnder1000 as select distinct CEMAIL, CNAME, ONO 
+from OurOrders natural join OurCustomers as CusAndOrd 
+where ONO not in 
+(select ONO from OurOrders natural join OurProducts where PRICE < 1000);
